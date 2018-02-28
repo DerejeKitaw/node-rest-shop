1bd90f8b983088b2b3ff92adeef711aa0320249a
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-27 20:27:23 -0500

    use morgan in the app

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 // excute express
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev')); // need to be befor the route
 
 // add middle ware
 app.use('/products', productRoutes);
