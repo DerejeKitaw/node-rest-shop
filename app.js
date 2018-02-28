ff575067550115986d85c83e9951d5ad45e0afff
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-27 20:10:32 -0500

    forward order rout to products

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 // add middle ware
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
