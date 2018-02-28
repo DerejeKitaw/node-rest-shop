ba119be6890ddc0d1f7ad7eb195cff48e006ef06
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-27 20:10:02 -0500

    create order route

---
+++ api/routes/orders.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Orders was created'
+    });
+});
+// Get information about individual order
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order detail',
+        orderId: req.params.orderId
+    });
+});
+// Delete individual order
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order deleted'
+    });
+});
+module.exports = router;
